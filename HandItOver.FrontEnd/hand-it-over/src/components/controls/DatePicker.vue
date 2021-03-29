fa101adde868ad2ab382e46717b3aa8eeea45e92
EdShel <eduardchelemetjev@gmail.com>
2021-03-29 18:11:15 +0300

    Date picker component

---
+++ HandItOver.FrontEnd/hand-it-over/src/components/controls/DatePicker.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="dates-container">
+    <div
+      class="date"
+      v-bind:class="{ selected: selectedDateIndex == i }"
+      v-for="(day, i) in getDays()"
+      :key="i"
+      v-on:click="changeSelectedDate(i)"
+    >
+      <div class="day-of-week">{{ getDayOfWeekShort(day) }}</div>
+      <div class="day-of-month">{{ day.getDate() }}</div>
+      <div class="month">{{ getMonthShort(day) }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DatePicker",
+  props: {
+    daysForwardCount: Number,
+  },
+  data() {
+    return {
+      selectedDateIndex: 0,
+    };
+  },
+  computed: {
+    date() {
+      let now = new Date();
+      now.setDate(now.getDate() + this.selectedDateIndex);
+      return now;
+    },
+  },
+  methods: {
+    *getDays() {
+      let dateItem = new Date();
+      for (let i = 0; i < this.daysForwardCount; i++) {
+        yield dateItem;
+        dateItem.setDate(dateItem.getDate() + 1);
+      }
+    },
+    getDayOfWeekShort(date) {
+      return daysOfWeek[date.getDay()];
+    },
+    getMonthShort(date) {
+      return months[date.getMonth()];
+    },
+    changeSelectedDate(selectedIndex) {
+      this.selectedDateIndex = selectedIndex;
+    },
+  },
+};
+
+const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "June",
+  "July",
+  "Aug",
+  "Sept",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+</script>
+
+<style scoped>
+.dates-container {
+  display: flex;
+  width: 100%;
+  overflow-x: scroll;
+}
+
+.date {
+  position: relative;
+  min-width: 80px;
+  flex-grow: 0;
+  flex-basis: 0;
+  border: 1px solid black;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 5px;
+  cursor: pointer;
+}
+
+.date.selected {
+  background-color: rgb(138, 168, 212);
+}
+
+.day-of-week {
+  align-self: flex-start;
+}
+
+.day-of-month {
+  align-self: center;
+  font-size: 1.6em;
+  font-weight: bold;
+  margin: 0 10px;
+}
+
+.month {
+  align-self: flex-end;
+}
+</style>
--- HandItOver.FrontEnd/hand-it-over/src/components/rent/RentPage.vue
+++ HandItOver.FrontEnd/hand-it-over/src/components/rent/RentPage.vue
@@ -1,18 +1,80 @@
 <template>
   <div>
-      {{ groupId }}
+    <h3>Rent options</h3>
+    <div v-if="group">
+      <div>
+        <label for="packageSizeSelect">Package size</label>
+        <select id="packageSizeSelect" v-model="rentInfo.packageSize">
+          <option value="0">Small</option>
+          <option value="1">Medium</option>
+          <option value="2">Large</option>
+        </select>
+      </div>
+      <div>
+        <label for="rentFromDate">Rent start</label>
+        <input id="rentFromDate" type="datetime" v-model="rentInfo.rentFrom" />
+        <date-picker v-bind:daysForwardCount="Number(14)"></date-picker>
+      </div>
+      <div>
+        <label for="rentDurationRange">Rent duration (minutes)</label>
+        <input
+          type="range"
+          min="15"
+          v-bind:max="group.maxRentMinutes"
+          step="5"
+          v-model="rentInfo.rentDurationMinutes"
+        />
+      </div>
+      <div>
+        <button v-on:click="renting">Rent</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import api from "~/util/api";
+import date from "~/util/date";
+import DatePicker from "~/components/controls/DatePicker";
+
 export default {
-    name: 'RentPage',
-    props: {
-        groupId: String
-    }
-}
+  name: "RentPage",
+  props: {
+    groupId: String,
+  },
+  components: {
+    DatePicker
+  },
+  data() {
+    return {
+      group: {},
+      rentInfo: {
+        packageSize: 1,
+        rentFrom: new Date(),
+        rentDurationMinutes: 120,
+      },
+    };
+  },
+  mounted() {
+    api
+      .sendGet(`/mailboxGroup/${this.groupId}`)
+      .then((r) => {
+        let data = r.data;
+        this.group = {
+          name: data.name,
+          maxRentMinutes: date.hhMmSsToSeconds(data.maxRentTime) / 60,
+          whitelistOnly: data.whitelistOnly,
+        };
+      })
+      .catch((e) => {});
+  },
+  methods: {
+    renting() {
+      console.log("Renting mailbox");
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
