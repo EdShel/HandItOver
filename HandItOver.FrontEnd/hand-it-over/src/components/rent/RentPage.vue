171e9c056f8c9f562a8af8b3c018e55680f876a0
EdShel <eduardchelemetjev@gmail.com>
2021-04-01 12:24:09 +0300

    Ability to delete rents

--- HandItOver.FrontEnd/hand-it-over/src/components/rent/RentPage.vue
+++ HandItOver.FrontEnd/hand-it-over/src/components/rent/RentPage.vue
@@ -1,9 +1,29 @@
 <template>
-  <div v-if="rent">{{ rent.rentId }} - {{ rent.from }} - {{ rent.until }}</div>
+  <div v-if="rent">
+    <p v-if="rent.mailbox">
+      <b>Mailbox: </b><span>{{ rent.mailbox.address }}</span>
+    </p>
+    <p v-if="rent.renter">
+      <b>Renter: </b>
+      <router-link v-bind:to="`/account/${rent.renter.id}`">
+        {{ rent.renter.fullName }} ({{ rent.renter.email }})
+      </router-link>
+    </p>
+    <p>
+      <b>Start time: </b><span>{{ formatDate(rent.from) }}</span>
+    </p>
+    <p>
+      <b>End time: </b><span>{{ formatDate(rent.until) }}</span>
+    </p>
+    <div>
+      <button v-on:click="onDeleteRentPressed">Delete rent</button>
+    </div>
+  </div>
 </template>
 
 <script>
 import api from "~/util/api";
+import dateUtil from "~/util/date";
 
 export default {
   name: "RentPage",
@@ -22,6 +42,15 @@ export default {
         this.rent = r.data;
       })
       .catch((e) => {});
+  },
+  methods: {
+    async onDeleteRentPressed() {
+      await api.sendDelete(`/mailboxGroup/rent/${this.rent.rentId}`);
+      this.$router.back();
+    },
+    formatDate(dateStr) {
+      return dateUtil.localString(new Date(dateStr));
+    },
   },
 };
 </script>
